b18f5e75842b128ea45750926fd8e89bad1ba509
mohit kumar <vishalkumar123mk@gmail.com>
2021-04-03 01:43:49 +0530

    tinder-clone add

--- src/projects.js
+++ src/projects.js
@@ -1,15 +1,21 @@
 const project=[
-    {
+        {
         id:1,
+        img:"https://images.pexels.com/photos/5053846/pexels-photo-5053846.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=650&w=940",
+        title:"Tinder-clone",
+        url:"https://focused-bohr-663e29.netlify.app/"
+    },    
+     {
+        id:2,
         img:"https://images.pexels.com/photos/270408/pexels-photo-270408.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
         title:"Basic-webite",
         url:"https://pedantic-swanson-01bcea.netlify.app"
     },
     {
-        id:2,
+        id:3,
         img:"https://images.pexels.com/photos/4065400/pexels-photo-4065400.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
         title:"Review-website",
         url:"https://6038904350b25b6306b9df93--blissful-gates-6aee5e.netlify.app"
-    }
+    },
     ]
     export default project;
